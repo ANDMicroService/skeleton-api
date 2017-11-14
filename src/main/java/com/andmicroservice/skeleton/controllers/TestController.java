d582867ca23d4e8fc9fd0c1bf402dd6da01f03ac
Tiago Goncalves <tgoncalves@and.digital>
2017-11-14 14:10:05 +0000

    Fixing sonarqube issues

--- src/main/java/com/andmicroservice/skeleton/controllers/TestController.java
+++ src/main/java/com/andmicroservice/skeleton/controllers/TestController.java
@@ -1,21 +1,22 @@
 package com.andmicroservice.skeleton.controllers;
 
-import com.andmicroservice.skeleton.SkeletonApplication;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @RestController
 public class TestController {
-    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    @GetMapping("/test")
-    @ResponseBody
-    public String test() {
-        logger.info("RestController is up and running...");
+  private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-        return "RestController is up and running!";
-    }
+  @GetMapping("/test")
+  @ResponseBody
+  public String test() {
+    logger.info("RestController is up and running...");
+
+    return "RestController is up and running!";
+  }
 }
